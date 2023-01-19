422edee55f73a8accebdfa6a999e657b58a8fd1e
Amit Hauzer <amit.hauzer@gmail.com>
2023-01-19 20:48:59 +0200

    test

--- shop/settings.py
+++ shop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-06_kt+wob)3-5=^!x(!*870g-433(fvvztd#av(7!a#767u(5f'
+SECRET_KEY = 'lkauhfgiouhLIUGWADUHwadiuuhqaw039or9(*&3*870g-433(fvvztd#av(7!a#767u(5f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'product',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,9 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    # "http://127.0.0.1:3000",
+]
