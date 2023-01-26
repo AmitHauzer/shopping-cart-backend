4d54397ebeb84bfea92404017706e125025cc33a
Amit Hauzer <amit.hauzer@gmail.com>
2023-01-26 19:21:23 +0200

    add users app

--- shop/urls.py
+++ shop/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('product.urls'))
+    path('products/', include('product.urls')),
+    path('login/', views.LoginView.as_view()),
 ]
