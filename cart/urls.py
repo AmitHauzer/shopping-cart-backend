4411f7521387c479cc30ebd77fe52e95aaeaa5e6
Amit Hauzer <amit.hauzer@gmail.com>
2023-02-07 22:13:56 +0200

    refactor: fix URLs

--- cart/urls.py
+++ cart/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.cart),       # customer_pk will be needed
-    path('cartitems/<cart_pk>/', views.get_cart_items),
-    path('cartitems/<cart_pk>/add/', views.add_item_to_cart),
-    path('cartitems/<cart_pk>/delete/', views.delete_item_from_cart),
-    path('cartitems/<cart_pk>/update/', views.update_item_in_cart),
+    path('<cart_pk>/cartitems/', views.get_cart_items),
+    path('cartitems/add/', views.add_item_to_cart),
+    path('cartitems/delete/', views.delete_item_from_cart),
+    path('cartitems/update/', views.update_item_in_cart),
 ]
