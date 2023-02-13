cb1d05b90769571a0f5dcf9d222685fc2bfca91e
Amit Hauzer <amit.hauzer@gmail.com>
2023-02-13 20:19:12 +0200

    refactor: API URLs

--- cart/urls.py
+++ cart/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.cart),       # customer_pk will be needed
-    path('<cart_pk>/cartitems/', views.get_cart_items),
-    path('cartitems/add/', views.add_item_to_cart),
-    path('cartitems/delete/', views.delete_item_from_cart),
-    path('cartitems/update/', views.update_item_in_cart),
+    path('<cart_pk>/cartitems/', views.all_cart_items),
+    path('<cart_pk>/cartitems/<product_id>', views.single_cartitem),
 ]
